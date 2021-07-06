fa8fa424370dbdabf9d48c8ec1f6b978507473ba
preyasu-rakshit <preyasu.official@gmail.com>
2021-07-06 16:13:12 +0530

    gave appropriate names

--- gray-scott-pygame.py
+++ gray-scott-pygame.py
@@ -1,30 +1,36 @@
+import sys
 from math import floor
 
 import numpy as np
+import pygame
 from numba import njit
-from PIL import Image
 
-# setup canvas
-width = 600
-height = 600
-seed_size = 40
+# Pygame variables
+width = 200
+height = 200
+screen_size = (width, height)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Reaction - Diffusion")
+clock = pygame.time.Clock()
+
+# setup seed size
+seed_size = 20
 
 # Diffusion Rates
 dA = 1.0
 dB = 0.5
 
-f = 0.037  # Feed Rate
-k = 0.06  # Kill Rate
+f = 0.018  # Feed Rate
+k = 0.051  # Kill Rate
 
 dt = 1  # time step
+
 
 # Initialize grids that store the value of cocentration of chemicals in each pixel
 current_grid = np.zeros((width, height, 2), dtype=np.float32)
 next_grid = np.zeros((width, height, 2), dtype=np.float32)
 
-for x in range(width):
-    for y in range(height):
-        current_grid[x, y] = [1, 0]
+current_grid[0:width, 0:height] = [1, 0]
 
 a = floor(width/2 - seed_size/2)
 b = floor(width/2 + seed_size/2)
@@ -32,8 +38,9 @@ c = floor(height/2 - seed_size/2)
 d = floor(height/2 + seed_size/2)
 current_grid[a:b, c:d] = [0, 1]
 
-
 # functions
+
+
 @njit
 def laplace2DA(x, y, grid):
     result = 0
@@ -73,6 +80,7 @@ def constrain(value, min_limit, max_limit):
 def update(c_grid):
     '''Updates the surface'''
 
+    # set color of each pixel according to concentration
     new_arr = np.zeros((width, height, 2), dtype=np.float32)
     for x in range(1, width - 1):
         for y in range(1, height - 1):
@@ -90,8 +98,6 @@ def update(c_grid):
 
 @njit
 def get_color(grid):
-    '''set color of each pixel according to concentration'''
-
     c_arr = np.zeros((width, height, 3), dtype=np.uint8)
     for x in range(width):
         for y in range(height):
@@ -105,19 +111,18 @@ def get_color(grid):
     return c_arr
 
 
-# main loop
-frames = 10000  # Number of frames desired by user
-num = 0
-for frame in range(frames):
+# Main loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
     next_grid = update(current_grid)
     canvas = get_color(next_grid)
+    display_surf = pygame.surfarray.make_surface(canvas)
     current_grid = next_grid
 
-    # generates image every fourth frame, change according to your needs
-    if frame % 4 == 0:
-        img = Image.fromarray(np.rot90(canvas), 'RGB')
-        str_num = "0000" + str(num)
-        img.save(f'video\\pic{str_num[-5:]}.png')
-        num += 1
-
-    print(frame)
+    screen.blit(display_surf, (0, 0))
+    pygame.display.flip()
+    # print(clock.get_fps())
+    clock.tick()
